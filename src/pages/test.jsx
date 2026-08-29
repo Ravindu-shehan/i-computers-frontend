e923dd9a96a793eaa9dc7dfe60da2774b65fd857
Ravindu-Shehan <ravindushehan200122@gmail.com>
2026-08-29 09:44:12 +0530

    feat: add axios dependency and implement login functionality with state management

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -3,12 +3,19 @@ import { useState } from "react"
 export default function Test(){
 
     const [count, setCount] = useState(0)
+    const [isVisible, setIsVisible] = useState(true)
     
 
     
     return(
         <div className="w-full h-full bg-accent flex items-center justify-center">
-            <div className="w-100 h-100 bg-amber-50 flex items-center justify-center flex-col" >
+            <h1>{isVisible}</h1>
+            <button onClick={
+                ()=>{
+                    setIsVisible(!isVisible)
+                }
+            } className="w-[50px] h-[50px] bg-red-600 text-white">{isVisible?"x":"o" }</button>
+            { isVisible && <div className="w-100 h-100 bg-amber-50 flex items-center justify-center flex-col" >
                 <h1 className="text-[55px]">{count}</h1>
                 <div className="w-full h-[50px]  gap-2 flex justify-center items-center">
                     <button onClick={
@@ -27,7 +34,7 @@ export default function Test(){
                         Increment
                     </button>
                 </div>
-            </div>
+            </div>}
 
         </div>
     )
