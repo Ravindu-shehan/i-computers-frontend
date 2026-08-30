83001aa1100794defd21c731b1a065952092d009
Ravindu-Shehan <ravindushehan200122@gmail.com>
2026-08-30 23:57:09 +0530

    Add env file✏️

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import AdminPage from "./pages/admin";
 import HomePage from "./pages/home";
 import LoginPage from "./pages/login";
 import Test from "./pages/test";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return(
     <div className="w-full h-screen bg-primary text-secondary">
+      <Toaster position="top-right"/>
       <Routes>
         <Route path="/" element= {<HomePage/>}/>
         <Route path="/admin/*" element={<AdminPage/>}/>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,28 +1,56 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage(){
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
 
-    function login(){
+    /*function login(){
         console.log(email)
         console.log(password)
-        axios.post("http://localhost:3000/users/login",
+        axios.post("http://localhost:3000/users/login"),
              {
             email: email,
             password: password
         }
+    ).then(
+        (response)=>{
+            console.log(response)
+            console.log("Login successful")
+        }
+    ).catch(
+        (error)=>{
+            console.log(error)
+            console.log("Login failed")
+        }
     )
+    }*/
+
+    async function login(){
+        try{
+            const response = await axios.post(import.meta.env.VITE_API_URL + "/users/login",
+                {
+                    email : email,
+                    password : password
+                }
+            )
+            console.log(response)
+            toast.success("Login successful")
+
+        }catch(error){
+            console.log(error)
+            toast.error("Login Failed")
+        }
     }
     return(
         <div className="w-full h-full bg-[url(/bgnew.jpg)] bg-cover bg-center bg-no-repeat flex items-center justify-center">
             <div className="w-[50%] h-full flex flex-col items-center justify-center">
 
-                <img src="/logo.jpg" className="w-75 h-75 object-cover" />
-                <h1 className="text-4xl font-bold mt-5 text-white">I Computers</h1>
+                <img src="/logoNew.jpg" className="w-75 h-75 object-cover" />
+                <h1 className="text-4xl font-bold mt-5 text-white"></h1>
 
             </div>
             
