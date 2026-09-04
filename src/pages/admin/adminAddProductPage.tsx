e3358199a75086adcabbbf85ed9917833d0dbdb7
Ravindu-Shehan <ravindushehan200122@gmail.com>
2026-09-04 09:39:52 +0530

    complete AdminAddProductPage✏️

--- src/pages/admin/adminAddProductPage.tsx
+++ src/pages/admin/adminAddProductPage.tsx
@@ -7,8 +7,11 @@ export default function AdminAddProductPage(){
     const [productAltNames, setProductAltNames] = useState("");
     const [productPrice, setProductPrice] = useState(0);
     const [productLabelledPrice, setProductLabelledPrice] = useState(0);
+    const [category, setCategory] = useState("Others");
+    const [productBrand, setProductBrand] = useState("Standard");
+    const [productModel, setProductModel] = useState("");
+    const [productIsVisible, setProductIsVisible] = useState(true);
     const [productImage, setProductImage] = useState("");
-    const [productCategory, setProductCategory] = useState("");
     const [productStock, setProductStock] = useState(0);
 
     return(
@@ -18,7 +21,7 @@ export default function AdminAddProductPage(){
                 <input 
                     placeholder="EX: ID001" 
                     className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 flex-1 focus:outline-white"
-                    
+                    value={productId}
                     onChange={(e) => {setProductId(e.target.value)}}
                 />
             </div>
@@ -27,7 +30,7 @@ export default function AdminAddProductPage(){
                 <input 
                     placeholder="john doe" 
                     className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 flex-1 focus:outline-white"
-                    
+                    value={productName}
                     onChange={(e) => {setProductName(e.target.value)}}
                 />
             </div>
@@ -36,7 +39,7 @@ export default function AdminAddProductPage(){
                 <textarea 
                     placeholder="Text-here" 
                     className="border-4 border-accent rounded-[10px] h-[100px] p-2 m-2 flex-1 focus:outline-white"
-                    
+                    value={productDescription}
                     onChange={(e) => {setProductDescription(e.target.value)}}
                 />
             </div>
@@ -45,7 +48,7 @@ export default function AdminAddProductPage(){
                 <input 
                     placeholder="john doe" 
                     className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 flex-1 focus:outline-white"
-                    
+                    value={productAltNames}
                     onChange={(e) => {setProductAltNames(e.target.value)}}
                 />
             </div>
@@ -54,8 +57,8 @@ export default function AdminAddProductPage(){
                 <input 
                     placeholder="$50" 
                     className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 flex-1 focus:outline-white"
-                    
-                    onChange={(e) => {setProductPrice(parseFloat(e.target.value) || 0)}}
+                    value={productPrice}
+                    onChange={(e) => {setProductPrice(parseFloat(e.target.value) )}} type="number"
                 />
             </div>
             <div className="w-[50%] h-[120px] flex flex-col ">
@@ -63,13 +66,54 @@ export default function AdminAddProductPage(){
                 <input 
                     placeholder="$50" 
                     className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 flex-1 focus:outline-white"
-                    
-                    onChange={(e) => {setProductLabelledPrice(parseFloat(e.target.value) || 0)}}
+                    value={productLabelledPrice}
+                    onChange={(e) => {setProductLabelledPrice(parseFloat(e.target.value) )}} type="number"
                 />
             </div>
+             <div className="w-[25%] h-[120px] flex flex-col ">
+                <label className="text-xl font-bold m-2">Category</label>
+                <select 
+                    className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 flex-1 focus:outline-white"
+                    value={category}
+                    onChange={(e) => {setCategory(e.target.value)}} >
+                
+                    <option value="">Select a category</option>
+                    <option value="1">Electronics</option>
+                    <option value="2">Clothing</option>
+                    <option value="3">Home & Kitchen</option>
+                </select>
+            </div>
+            <div className="w-[25%] h-[120px] flex flex-col ">
+                <label className="text-xl font-bold m-2">Brand</label>
+                <input 
+                    placeholder="Apple"
+                    className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 flex-1 focus:outline-white"
+                    value={productBrand}
+                    onChange={(e) => {setProductBrand(e.target.value)}}
+                />
+            </div>
+            <div className="w-[25%] h-[120px] flex flex-col ">
+                <label className="text-xl font-bold m-2">Model</label>
+                <input 
+                    placeholder="inspiration-15"
+                    className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 flex-1 focus:outline-white"
+                    value={productModel}
+                    onChange={(e) => {setProductModel(e.target.value)}}
+                />
+            </div>
+            <div className="w-[25%] h-[120px] flex flex-col ">
+                <label className="text-xl font-bold m-2">Visible</label>
+                <select 
+                    className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 flex-1 focus:outline-white"
+                    value={productIsVisible.toString()}
+                    onChange={(e) => {setProductIsVisible(e.target.value === "true")}} > 
+                    <option value="false">No</option>
+                    <option value="true">Yes</option>
+                </select>
+            </div>
+            
             
 
-            </div>
         </div>
     )
 }
