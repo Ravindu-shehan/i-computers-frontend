8abbae8c6542bd5b88ac8c4d54f4aaabd73ed7e7
Ravindu-Shehan <ravindushehan200122@gmail.com>
2026-09-13 00:09:04 +0530

    Add Supabase client integration and file upload functionality in Test component✏️

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react"
+import { createClient } from "@supabase/supabase-js/dist/index.cjs"
+
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqaHRmeGdhYnNkaGZqeXBlaWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMjc3NjksImV4cCI6MjEwNDgwMzc2OX0.h-IaFqlouac03WJZHcrFRl6TC__LqLeEzK20s8z5Vwg"
+const supabaseUrl = "https://wjhtfxgabsdhfjypeikg.supabase.co/rest/v1/"
+
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default function Test(){
 
-    const [count, setCount] = useState(0)
-    const [isVisible, setIsVisible] = useState(true)
+    const [file , setFile] = useState(null)
+
+    function uploadFile(){
+        console.log(file)
+
+        supabase.storage.from("images").upload(file.name, file, {
+            upsert: false,
+            cacheControl : 3600
+        }).then(
+            ()=>{
+                const url = supabase.storage.from("images").getPublicUrl(file.name).data.publicUrl
+            }
+        )
+    }
     
 
     
@@ -12,9 +30,13 @@ export default function Test(){
            
            <input type="file" onChange={
             (e)=>{
-                console.log(e.target.files[0])
+                setFile(e.target.files[0])
             }
            }/>
+
+           <button onChange={uploadFile} className="w-[100px] h-[40px] bg-accent text-white rounded-lg">
+            Upload 
+           </button>
 
 
         </div>
