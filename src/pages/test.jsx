f673833c9d7ad036066775576fa9fccda6722ff2
Ravindu-Shehan <ravindushehan200122@gmail.com>
2026-09-12 23:34:28 +0530

    update file input handling✏️

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -9,32 +9,13 @@ export default function Test(){
     
     return(
         <div className="w-full h-full bg-accent flex items-center justify-center">
-            <h1>{isVisible}</h1>
-            <button onClick={
-                ()=>{
-                    setIsVisible(!isVisible)
-                }
-            } className="w-[50px] h-[50px] bg-red-600 text-white">{isVisible?"x":"o" }</button>
-            { isVisible && <div className="w-100 h-100 bg-amber-50 flex items-center justify-center flex-col" >
-                <h1 className="text-[55px]">{count}</h1>
-                <div className="w-full h-[50px]  gap-2 flex justify-center items-center">
-                    <button onClick={
-                        ()=>{
-                            setCount(count-1)
+           
+           <input type="file" onChange={
+            (e)=>{
+                console.log(e.target.files[0])
+            }
+           }/>
 
-                        }
-                    } className="w-[100px] h-[45px] bg-red-600 text-white">
-                        Decrement
-                    </button>
-                    <button onClick={
-                        ()=>{
-                             setCount(count+1)
-                        }
-                        }className="w-[100px] h-[45px] bg-green-600 text-white">
-                        Increment
-                    </button>
-                </div>
-            </div>}
 
         </div>
     )
--- utils/price-format.js
+++ utils/price-format.js
@@ -13,3 +13,6 @@ if(isNaN(priceInNumber)){
 }
 
 }
+
+//https://wjhtfxgabsdhfjypeikg.supabase.co/rest/v1/
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqaHRmeGdhYnNkaGZqeXBlaWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMjc3NjksImV4cCI6MjEwNDgwMzc2OX0.h-IaFqlouac03WJZHcrFRl6TC__LqLeEzK20s8z5Vwg
